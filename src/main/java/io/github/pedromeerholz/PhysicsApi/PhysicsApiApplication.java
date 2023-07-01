37b24992d40d5711c890e0ace2042e6747783d1e
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-07-01 16:54:39 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/io/github/pedromeerholz/PhysicsApi/PhysicsApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedromeerholz.PhysicsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicsApiApplication.class, args);
+	}
+
+}
