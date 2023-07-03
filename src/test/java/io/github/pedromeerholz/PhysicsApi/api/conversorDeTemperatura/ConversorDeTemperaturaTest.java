3b8546d81a8e8d89275ed79b0557171404d2df12
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-07-02 21:18:50 -0300

    Testes dos métodos de conversão de celcius para kelvin e fahrenheit para celcius

--- src/test/java/io/github/pedromeerholz/PhysicsApi/api/conversorDeTemperatura/ConversorDeTemperaturaTest.java
+++ src/test/java/io/github/pedromeerholz/PhysicsApi/api/conversorDeTemperatura/ConversorDeTemperaturaTest.java
@@ -4,8 +4,6 @@ import io.github.pedromeerholz.PhysicsApi.api.resultado.Resultado;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ConversorDeTemperaturaTest {
     private ConversorDeTemperatura conversorDeTemperatura = new ConversorDeTemperatura();
 
@@ -15,5 +13,21 @@ class ConversorDeTemperaturaTest {
         Resultado resultado = this.conversorDeTemperatura.converterCelciusParaFahrenheit(temperatura);
         float temperaturaFinal = resultado.getResultado();
         Assertions.assertEquals(89.6, temperaturaFinal, 2);
+    }
+
+    @Test
+    void converterCelciusParaKelvin() {
+        float temperatura = 50;
+        Resultado resultado = this.conversorDeTemperatura.converterCelciusParaKelvin(temperatura);
+        float temperaturaFinal = resultado.getResultado();
+        Assertions.assertEquals(323.15, temperaturaFinal, 2);
+    }
+
+    @Test
+    void converterFahrenheitParaCelcius() {
+        float temperatura = 200.5F;
+        Resultado resultado = this.conversorDeTemperatura.converterFahrenheitParaCelcius(temperatura);
+        float temperaturaFinal = resultado.getResultado();
+        Assertions.assertEquals(93.61, temperaturaFinal, 2);
     }
 }
