684ada8a8970a92bfd95b2583a423122fdde491d
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-07-07 16:14:35 -0300

    Removendo a extensão da classe PhysicsApiApplication

--- src/main/java/io/github/pedromeerholz/PhysicsApi/PhysicsApiApplication.java
+++ src/main/java/io/github/pedromeerholz/PhysicsApi/PhysicsApiApplication.java
@@ -2,10 +2,9 @@ package io.github.pedromeerholz.PhysicsApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhysicsApiApplication extends SpringBootServletInitializer {
+public class PhysicsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhysicsApiApplication.class, args);
