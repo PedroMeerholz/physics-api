2340797926a25f51659ca69181d09f44489b22ef
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-07-07 16:10:02 -0300

    Estendo PhysicsApiApplication de SpringBootServletInitializer

--- src/main/java/io/github/pedromeerholz/PhysicsApi/PhysicsApiApplication.java
+++ src/main/java/io/github/pedromeerholz/PhysicsApi/PhysicsApiApplication.java
@@ -2,12 +2,12 @@ package io.github.pedromeerholz.PhysicsApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhysicsApiApplication {
+public class PhysicsApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhysicsApiApplication.class, args);
 	}
-
 }
